f34130f70cfb98e479ef7f8baa1ed67547fa5de7
hboukiili <hboukili@student.1337.ma>
2024-04-23 21:24:19 +0100

    first push

---
+++ models_only/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from enum import Enum
+
+class users(models.Model):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    created_at = models.DateField(auto_now_add=True)
+
+class Agriculture(users):
+
+    test = models.CharField(max_length=50)
+
+class chercheur(users):
+
+    test = models.CharField(max_length=30)
+
+class Policy_Maker(users):
+
+    test = models.CharField(max_length=30)
+
+class field(models.Model):
+
+    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    boundaries = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+
+class saison(models.Model):
+
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    field_id = models.ForeignKey(field, on_delete=models.CASCADE)
+    rendement = models.IntegerField()
+
+class Soil_type(Enum):
+
+    Loam = "Loam"
+    Sandy = "Sandy soil"
+    Clay = "Clay"
+    Silty = "Silty"
+    Peat = "Peat soil"
+    Black = "Black soil"
+    Arid = "Arid soil"
+
+class soil(models.Model):
+
+    soil_type = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in Soil_type])
+    field_id = models.ForeignKey(field, on_delete=models.CASCADE)
+
+class irrigation_system(models.Model):
+
+    irrigation_type = models.CharField(max_length=50) # should be enum
+    debit = models.IntegerField()
+    instalation_date = models.DateField()
+    Maintenance_date = models.DateField()
+    field_id = models.ForeignKey(field, on_delete=models.CASCADE)
+
+class Data_source(models.Model):
+
+    datetime = models.DateField()
+    field_id = models.ForeignKey(field, on_delete=models.CASCADE)
+
+class Remote_sensing(Data_source):
+
+    path = models.CharField(max_length=50)
+    RS_type = models.CharField(max_length=50)
+    source = models.CharField(max_length=50)
+
+class station(Data_source):
+
+    coordinates = models.CharField(max_length=50)
+    station_type = models.CharField(max_length=20) # should be enum
+    sensor_type = models.CharField(max_length=20) # should be enum
+    mesur = models.IntegerField()
+
+class Ogimet_stations(models.Model):
+
+    id = models.IntegerField()
+    coordinates = models.CharField()
+    location_name = models.CharField()
